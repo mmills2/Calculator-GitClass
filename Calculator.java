22a13f49961f08d9c518720d170e31e16b84511f
mmills2 <30macm@gmail.com>
2023-02-07 19:19:15 -0500

    added scanner, prompt for user to enter two whole numbers, two variables to store user inputted numbers, and print statement of the sum of those two numbers

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,17 @@
-public class Calculator{
+import java.util.Scanner;
 
-    public static void main(String[] args){
-        
+public class Calculator {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter two whole numbers");
+
+        // this is to practice git and github so I'm not checking if the input is in the
+        // right form
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+
+        System.out.println(num1 + num2);
     }
 }
