67f2785437fbb42f8140a26772d623af6fbadc06
mmills2 <30macm@gmail.com>
2023-02-07 19:38:07 -0500

    added add or subsrtact choice with output reflected

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 
+// this is to practice git and github so I'm not checking if the input is in the right form
 public class Calculator {
 
     public static void main(String[] args) {
@@ -7,11 +8,18 @@ public class Calculator {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter two whole numbers");
 
-        // this is to practice git and github so I'm not checking if the input is in the
-        // right form
         int num1 = scanner.nextInt();
         int num2 = scanner.nextInt();
 
-        System.out.println(num1 + num2);
+        System.out.println("Would you like to add or subtract");
+
+        scanner.nextLine(); // for the /n after .nextInt()
+        String operation = scanner.nextLine();
+
+        if (operation.equalsIgnoreCase("add")) {
+            System.out.println(num1 + num2);
+        } else {
+            System.out.println(num1 - num2);
+        }
     }
 }
